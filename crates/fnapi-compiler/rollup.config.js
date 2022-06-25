279005acc36e5b91c84588d7c0686e30f769f6cc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-25 17:34:29 +0900

    typescript

--- crates/fnapi-compiler/rollup.config.js
+++ crates/fnapi-compiler/rollup.config.js
@@ -1,9 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
 
 
 export default {
-    input: 'src/type_server/index.js',
+    input: 'src/type_server/index.ts',
     output: {
         file: 'bundle.js',
         format: 'cjs'
-    }
+    },
+    plugins: [typescript()]
 };
