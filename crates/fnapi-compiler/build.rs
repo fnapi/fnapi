59794a823ac24111c27a3688eb906a423c1b32ad
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-25 18:31:48 +0900

    Build script

--- crates/fnapi-compiler/build.rs
+++ crates/fnapi-compiler/build.rs
@@ -4,9 +4,6 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/**/*.ts");
 
-    let status = Command::new("npx").arg("tsc").status().unwrap();
-    assert!(status.success());
-
     let status = Command::new("npx")
         .arg("rollup")
         .arg("-c")
