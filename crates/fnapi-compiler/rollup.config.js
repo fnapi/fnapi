61f871a6cb5870809dea7a8d9bb0532c45a0c908
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-25 17:39:47 +0900

    rollup

--- crates/fnapi-compiler/rollup.config.js
+++ crates/fnapi-compiler/rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-
+import json from "@rollup/plugin-json";
 
 export default {
     input: 'src/type_server/index.ts',
@@ -9,5 +9,5 @@ export default {
         file: 'bundle.js',
         format: 'cjs'
     },
-    plugins: [typescript(), nodeResolve(), commonjs()]
+    plugins: [typescript(), nodeResolve(), commonjs(), json()]
 };
