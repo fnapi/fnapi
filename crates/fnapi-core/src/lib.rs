f81bb7d8d5016f0aa339f98ce14f0e429226ebb1
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-26 20:44:26 +0900

    Impl

--- crates/fnapi-core/src/lib.rs
+++ crates/fnapi-core/src/lib.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{borrow::Cow, sync::Arc};
 
 use swc_common::{
     errors::{Handler, HANDLER},
@@ -16,4 +16,11 @@ impl Env {
     pub fn with<Ret>(&self, op: impl FnOnce() -> Ret) -> Ret {
         GLOBALS.set(&self.globals, || HANDLER.set(&self.handler, op))
     }
+}
+
+/// Any kind of implementation.
+pub trait Impl: Send + Sync {
+    fn id(&self) -> Cow<'static, str>;
+
+    fn name(&self) -> Cow<'static, str>;
 }
