6e7a6fd3e04f0ad12a2d0a35c85bc86c4d74a859
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-27 07:52:31 +0900

    Fix

--- crates/fnapi-compiler/rollup.config.js
+++ crates/fnapi-compiler/rollup.config.js
@@ -4,13 +4,9 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import * as path from "node:path";
 import { env } from "node:process";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
-  input: path.join(__dirname, "src/type_server/index.ts"),
+  input: path.join(env.CARGO_MANIFEST_DIR, "src/type_server/index.ts"),
   output: {
     file: path.join(env.OUT_DIR, "type-server.js"),
     format: "cjs",
