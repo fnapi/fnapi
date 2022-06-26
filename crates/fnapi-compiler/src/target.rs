db869cd4e29abeb48a2be386a0d3990f55ff230f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-26 20:03:26 +0900

    Try

--- crates/fnapi-compiler/src/target.rs
+++ crates/fnapi-compiler/src/target.rs
@@ -1,5 +1,13 @@
+use fnapi_api_def::{ApiFile, ProjectApis};
+
 /// The target of **server**.
-pub trait Target {}
+pub trait Target {
+    fn store_api(&self, http_path: &str, project: &ProjectApis, file: &ApiFile, api: &ApiFn);
+}
+
+pub struct Direct {}
+
+impl Target for Direct {}
 
 pub struct NextJs {}
 
