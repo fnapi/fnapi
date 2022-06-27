ab5b894cb75d8ec075b8de82793f8f0a05ce7b74
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-27 12:24:32 +0900

    chmod

--- crates/fnapi-compiler/build.rs
+++ crates/fnapi-compiler/build.rs
@@ -1,9 +1,18 @@
-use std::process::Command;
+use std::{env, process::Command};
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=rollup.config.js");
     println!("cargo:rerun-if-changed=src/**/*.ts");
+
+    if !cfg!(target_os = "windows") && env::var("CI").as_deref() == Ok("1") {
+        Command::new("chmod")
+            .arg("-R")
+            .arg("777")
+            .arg(env::var("OUT_DIR").unwrap())
+            .status()
+            .unwrap();
+    }
 
     let mut c = if cfg!(target_os = "windows") {
         let mut c = Command::new("cmd");
