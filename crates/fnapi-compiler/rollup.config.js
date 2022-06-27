9d1af4090239be2ffafe9433d1ff1e223136a4b5
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-27 10:33:27 +0900

    Fuck

--- crates/fnapi-compiler/rollup.config.js
+++ crates/fnapi-compiler/rollup.config.js
@@ -6,9 +6,9 @@ import * as path from "node:path";
 import { env } from "node:process";
 
 export default {
-  input: path.join(env.CARGO_MANIFEST_DIR, "src/type_server/index.ts"),
+  input: path.join(env.CARGO_MANIFEST_DIR || ".", "src/type_server/index.ts"),
   output: {
-    file: path.join(env.OUT_DIR, "type-server.js"),
+    file: path.join(env.OUT_DIR || ".", "type-server.js"),
     format: "cjs",
   },
   external: ["ts-morph"],
