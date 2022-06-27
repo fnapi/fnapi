f3039c76e84c2cea435ef157792a7ef63e52593f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-27 19:38:47 +0900

    Check

--- packages/api/src/magic.ts
+++ packages/api/src/magic.ts
@@ -1,15 +1,24 @@
-
+import { FnApiReply, FnApiRequest } from "./rt/types";
 
 /**
  * Request context. This is a magic type processed by the compiler, and does not exist on runtime.
  */
 export declare class Context {
-    static get<T = never>(): T;
+  static get<T>(provider: Provider<T>): T;
 }
+
+export class Provider<T> {
+  #op: ProvideFn<T>;
+  #symbol: unique symbol;
+}
+
+export function Provide<T>(op: ProvideFn<T>): Provider<T> {}
+
+export type ProvideFn<T> = (req: FnApiRequest, res: FnApiReply) => Promise<T>;
 
 /**
  * Request context. This is a magic type processed by the compiler, and does not exist on runtime.
  */
 export declare class ServerConfig {
-    static get<T = never>(): T;
+  static get<T = never>(): T;
 }
