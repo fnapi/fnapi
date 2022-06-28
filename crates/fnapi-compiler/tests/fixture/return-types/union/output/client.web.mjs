18ad6f3d9a5f3a68a7e321e95e77e8c8077ef4b7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-28 22:22:37 +0900

    Update test refs

--- crates/fnapi-compiler/tests/fixture/return-types/union/output/client.web.mjs
+++ crates/fnapi-compiler/tests/fixture/return-types/union/output/client.web.mjs
@@ -1,4 +1,4 @@
-import __client from "@fnapi/api/client/web.js";
+import __client from "@fnapi/api/lib/client/web.js";
 export const TestApi = {
     async test () {
         return __client.invoke("TestApi", "test", arguments);
