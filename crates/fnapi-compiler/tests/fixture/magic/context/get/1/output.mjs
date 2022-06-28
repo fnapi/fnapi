809217d8b0084e1798205678287112875168da75
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-28 19:05:20 +0900

    Oh

--- crates/fnapi-compiler/tests/fixture/magic/context/get/1/output.mjs
+++ crates/fnapi-compiler/tests/fixture/magic/context/get/1/output.mjs
@@ -1,12 +1,12 @@
 import wrapApiClass from "@fnapi/api/rt/wrapApiClass.js";
 import wrapFnApiConfig from "@fnapi/api/rt/wrapFnApiConfig.js";
 const __fnapi_config_for_test = wrapFnApiConfig();
-import '@fnapi/api';
+import { provide } from "@fnapi/api";
 export default wrapApiClass(class TestApi {
     static async test(_req, _reply) {
         const params = _req.params;
         let arg1 = params[0];
-        const user = _req.getContext(User);
+        const user = UserProvider.get(_req, _reply);
     }
 }, [
     {
@@ -18,3 +18,4 @@ export default wrapApiClass(class TestApi {
         returnType: JSON.parse('{"type":"string"}')
     }
 ]);
+const UserProvider = provide();
