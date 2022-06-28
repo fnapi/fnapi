64df513a7db9607de31ac1b4f5229116e078bd04
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-28 22:09:34 +0900

    Rename

---
+++ packages/api/lib/api.ts
@@ -0,0 +1,11 @@
+export * from "./api/param.js";
+
+export function FnApi(): MethodDecorator {
+  return (prototype, key, descriptor) => {};
+}
+
+export interface HttpApiOptions {}
+
+export function Http(options: HttpApiOptions): MethodDecorator {
+  return (prototype, key, descriptor) => {};
+}
--- packages/api/src/rt/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as createServer } from "./createServer.js"
-export { default as wrapApiClass } from "./wrapApiClass.js"
-export { default as wrapFnApiConfig } from "./wrapFnApiConfig.js"
